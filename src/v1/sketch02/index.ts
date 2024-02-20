db90ef4ed4377b67d1d1d79489cfdb988c4a50d2
Akifumi Nakamura <a-nakamura@studio-spoon.co.jp>
2024-02-21 02:47:55 +0900

    sketch2の作成（回転）

---
+++ src/v1/sketch02/index.ts
@@ -0,0 +1,72 @@
+import * as THREE from 'three';
+import { ThreeBase } from './ThreeBase';
+import rawVertexShader from './raw_triangle.vs';
+import rawFragmentShader from './raw_triangle.fs';
+
+class Canvas extends ThreeBase {
+  private triangle: THREE.Mesh<
+    THREE.BufferGeometry,
+    THREE.ShaderMaterial,
+    THREE.Object3DEventMap
+  >;
+
+  constructor(canvas: HTMLCanvasElement) {
+    super(canvas, true);
+    this.init();
+    this.triangle = this.createTriangle();
+  }
+
+  private init() {
+    this.scene.background = new THREE.Color('#111');
+    this.camera.position.set(0, 0, 5);
+    this.camera.lookAt(0, 0, 0);
+  }
+
+  private createTriangle() {
+    const geometry = new THREE.BufferGeometry();
+    const r3 = 1.7320508;
+
+    // prettier-ignore
+    const vertices = [
+			-1.0,	-r3 / 3,		0.0,
+			1.0,	-r3 / 3,		0.0,
+			0.0,	2 * r3 / 3,	0.0
+		]
+
+    // prettier-ignore
+    const uv = [
+      0.0, 1.0,
+      1.0, 1.2,
+      0.8, 0.6
+    ]
+
+    geometry.setAttribute(
+      'position',
+      new THREE.Float32BufferAttribute(vertices, 3)
+    );
+    geometry.setAttribute('uv', new THREE.Float32BufferAttribute(uv, 2));
+
+    const material = new THREE.RawShaderMaterial({
+      uniforms: {
+        uTime: { value: 0 },
+      },
+      vertexShader: rawVertexShader,
+      fragmentShader: rawFragmentShader,
+    });
+
+    console.log(geometry);
+
+    const mesh = new THREE.Mesh(geometry, material);
+    this.scene.add(mesh);
+    return mesh;
+  }
+
+  protected render() {
+    this.updateTime();
+    this.camera.position.z += this.time.delta * -0.3;
+    this.triangle.rotation.z += this.time.delta * this.time.elapsed;
+    super.render();
+  }
+}
+
+new Canvas(document.querySelector<HTMLCanvasElement>('canvas')!);
